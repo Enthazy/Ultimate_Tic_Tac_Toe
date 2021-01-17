b036ba12da1147dc86233dc861f9f8e571f9ea9d
Enthazy <stephenx.wang@mail.utoronto.ca>
2021-01-17 00:58:05 -0500

    init

--- GameWithAI/src/GUI/RuleTextBox.java
+++ GameWithAI/src/GUI/RuleTextBox.java
@@ -1,3 +1,17 @@
+//Copyright 2015 Baiwei Chen, Hanchun Wang
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
 package GUI;
 
 import javax.swing.*;
@@ -43,7 +57,11 @@ public class RuleTextBox extends JFrame {
                 "local board at the top right of the global board. O can then play in any one of<BR>" +
                 "the nine available spots in that local board, each move sending X to a different local board.<BR><BR>" +
                 "Rule of wining:<BR>" +
-                "When any three move are adjacent in a line that is either horizontal, vertical or diagonal.<BR>";
+                "When any three move are adjacent in a line that is either horizontal, vertical or diagonal.<BR>" +
+                "<BR>" +
+                "This program is developed by Baiwei Chen and Hanchun Wang in 2015.<BR>" +
+                "Version 0.25<BR>";
+
         _label = new JLabel(message, JLabel.CENTER);
     }
 
